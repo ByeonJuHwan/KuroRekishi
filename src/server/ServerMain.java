499357894e60db001f460a67b96336efbbc5ec84
juhwan <asdcz@naver.com>
2022-10-05 02:19:26 +0900

    ServerMai 클래스 완성

---
+++ src/server/ServerMain.java
@@ -0,0 +1,31 @@
+package server;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerMain {
+    public static ArrayList<MultiServerThread> list;
+    public static Socket socket;
+    
+
+    public static void main(String[] args) {
+        list = new ArrayList<>();
+        try {
+            ServerSocket serverSocket = new ServerSocket(); 
+            serverSocket.bind(new InetSocketAddress("127.0.0.1", 5000));
+            MultiServerThread multiServerThread = null;
+            boolean isStop = true;
+            System.out.println("서버 정상 실행중...");
+            while(isStop) {
+                socket = serverSocket.accept();
+                multiServerThread = new MultiServerThread();
+                list.add(multiServerThread);
+                multiServerThread.start();
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
