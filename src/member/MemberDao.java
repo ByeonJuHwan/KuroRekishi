eeb6e11978c2c36b9a531f65baac1730baa24085
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2022-10-04 23:11:17 +0900

    인터페이스 설정 완료

--- src/member/MemberDao.java
+++ src/member/MemberDao.java
@@ -1,5 +1,29 @@
 package member;
 
 public interface MemberDao {
-
+	
+	
+	/**
+	 * 회원가입시 아이디 중복확인을 위한 메서드.
+	 * @param id 회원가입시 원하는 아이디 입력.
+	 * @return 아이디가 존재하면 true, 아이디가 존재하지 않아서 사용할수 있으면 false.
+	 */
+	public boolean findMember(String id);
+	
+	/**
+	 * 새로운 멤버 객체를 받아서 저장
+	 * @param member
+	 * @return 성공하면 1을 리턴, 실패시 0리턴
+	 */
+	public int addmember(Member member);
+	
+	
+	/**
+	 * 멤버 객체를 받아서 멤버 정보 변경
+	 * @param member 업데이트할 정보
+	 * @return 업데이트 성공하면 1, 그렇지 않으면 0.
+	 */
+	public int updateMember(Member member);
+	
+	
 }
--- src/member/MemberDaoImpl.java
+++ src/member/MemberDaoImpl.java
@@ -2,4 +2,22 @@ package member;
 
 public class MemberDaoImpl implements MemberDao{
 
+	@Override
+	public boolean findMember(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int addmember(Member member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateMember(Member member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
