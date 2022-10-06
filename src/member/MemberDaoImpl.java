0b8756798a663b98e64b17f4310030ff921f44f9
ByeonJuHwan <asdcz@naver.com>
2022-10-06 09:51:27 +0900

    memberDaoImpl 싱글톤 패턴 설정 - TODO 확인

--- src/member/MemberDaoImpl.java
+++ src/member/MemberDaoImpl.java
@@ -2,7 +2,27 @@ package member;
 
 public class MemberDaoImpl implements MemberDao{
 
-	@Override
+	// singleton
+    // 1. 자기자신 타입의 private static인 변수 선언
+    private static MemberDaoImpl instance = null;
+    // 2. private 생성자
+    private MemberDaoImpl () {
+        //TODO 연락처 프로그램 fileutil 패키지 참고해서 이미지 넣기랑 파일 만들기 참고.
+        
+    }
+    // 3. 자기 자신을 리턴하는 public static 메서드 정의
+    public static MemberDaoImpl getInstance() {
+        if(instance == null) {
+            instance  = new MemberDaoImpl();
+        }
+        return instance;
+    }
+    
+    
+    
+    
+    
+    @Override
 	public boolean findMember(String id) {
 		// TODO Auto-generated method stub
 		return false;
